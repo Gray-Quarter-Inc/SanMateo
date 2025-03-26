d04e0f2b7d87e466847a86627d22fc6d2e194e42
Deanna Hoops <deanna@grayquarter.com>
2025-03-26 06:55:52 -0400

    Update formatAddressAndOwnerZips.js

--- INCLUDES_CUSTOM/formatAddressAndOwnerZips.js
+++ INCLUDES_CUSTOM/formatAddressAndOwnerZips.js
@@ -1,50 +1,40 @@
-function formatAddressAndOwnerZips(recordId) {
-    var altId = aa.cap.getCap(recordId).getOutput().getCapModel().altID;
-    logDebug("formatAddressAndOwnerZips(" + altId + ")");
-
-    if (!recordId) {
-        logDebug("Invalid capId provided");
-        return;
-    }
-
-    capId = recordId;
-
+function formatAddressAndOwnerZips() {
+   
     var allAddressModels = aa.address.getAddressByCapId(capId).getOutput();
 
-    for (var i = 0; i < allAddressModels.length; i++) {
-        var addressModel = allAddressModels[i];
-        var zip = addressModel.zip;
+    if (allAddressModels && allAddressModels.length > 0) {
 
-        zip = formatZip(zip);
+        for (var i = 0; i < allAddressModels.length; i++) {
+            var addressModel = allAddressModels[i];
+            var zip = addressModel.zip;
 
-        addressModel.zip = zip;
-        var result = aa.address.editAddress(addressModel);
-        if (result.getSuccess() == true) {
-            var number = i + 1;
-            logDebug("Address " + number + " Zip code updated");
+            zip = formatZip(zip);
+
+            addressModel.zip = zip;
+            var result = aa.address.editAddress(addressModel);
+            if (result.getSuccess() == true) {
+                var number = i + 1;
+                logDebug("Address " + number + " Zip code updated");
+            }
         }
     }
 
     var allCapOwnerModels = aa.owner.getOwnerByCapId(capId).getOutput();
 
-    for (i = 0; i < allCapOwnerModels.length; i++) {
-        var capOwnerModel = allCapOwnerModels[i];
-        var zip = capOwnerModel.mailZip;
+    if (allCapOwnerModels && allCapOwnerModels.length > 0) {
+        for (i = 0; i < allCapOwnerModels.length; i++) {
+            var capOwnerModel = allCapOwnerModels[i];
+            var zip = capOwnerModel.mailZip;
 
-        zip = formatZip(zip);
+            zip = formatZip(zip);
 
-        capOwnerModel.mailZip = zip;
-        var result = aa.owner.updateDailyOwnerWithAPOAttribute(capOwnerModel);
-        if (result.getSuccess() == true) {
-            var number = i + 1;
-            logDebug("Owner " + number + " Zip code updated");
+            capOwnerModel.mailZip = zip;
+            var result = aa.owner.updateDailyOwnerWithAPOAttribute(capOwnerModel);
+            if (result.getSuccess() == true) {
+                var number = i + 1;
+                logDebug("Owner " + number + " Zip code updated");
+            }
         }
     }
 }
-/*******************************************************
-| Script/Function: formatZip(zip)
-| Created by: Nicolaj Bunting
-| Created on: 31May17
-| Usage: Formats a provided Zip code to use the 9 digit "#####-####" format
-| Modified by: ()
-*********************************************************/
+
