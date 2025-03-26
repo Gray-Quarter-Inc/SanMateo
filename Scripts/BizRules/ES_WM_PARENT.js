f9fd5725fbb06d4bff04ea53a0e664402658dcd6
Deanna Hoops <deanna@grayquarter.com>
2025-03-26 06:32:42 -0400

    Update ES_WM_PARENT.js

--- Scripts/BizRules/ES_WM_PARENT.js
+++ Scripts/BizRules/ES_WM_PARENT.js
@@ -6,24 +6,29 @@ ApplicationSubmitAfter
 ConvertToRealCAPAfter
  */
 
-if (AInfo['Associated Building Permit Number']) {
+if (AInfo['Associated Building Permit Number'] && AInfo['Associated Building Permit Number'] != "") {
     addParent(AInfo['Associated Building Permit Number']);
+
+    parentID = getApplication("" + AInfo['Associated Building Permit Number']);
+    if (parentID) {
+        copyParcels(parentID, capId);
+        copyAddresses(parentID, capId);
+        copyContacts(parentID, capId);
+        copyLicensedProf(parentID, capId);
+        copyOwner(parentID, capId);
+        updateAppStatus('Submitted', 'Initial Status');
+        editAppName('WMP for ' + AInfo['Associated Building Permit Number']);
+        editAppSpecific('WMP required because project is a', getAppSpecific('Waste Management Plan', parentID));
+        if (matches(getAppSpecific('Green Building?', parentID), '50 Percent', 'COM', 'SFD')) {
+            editAppSpecific('Green Building', 'Yes');
+        } else {
+            editAppSpecific('Green Building', 'No');
+        }
+    
+        pWorkDesc = workDescGet(parentID);
+        updateWorkDesc(pWorkDesc, capId);
+    }
+
 }
 
-parentID = getParent();
-copyParcels(parentID, capId);
-copyAddresses(parentID, capId);
-copyContacts(parentID, capId);
-copyLicensedProf(parentID, capId);
-copyOwner(parentID, capId);
-updateAppStatus('Submitted', 'Initial Status');
-editAppName('WMP for ' + AInfo['Associated Building Permit Number']);
-editAppSpecific('WMP required because project is a', getAppSpecific('Waste Management Plan', parentID));
-if (matches(getAppSpecific('Green Building?', parentID), '50 Percent', 'COM', 'SFD')) {
-    editAppSpecific('Green Building', 'Yes');
-} else {
-    editAppSpecific('Green Building', 'No');
-}
 
-pWorkDesc = workDescGet(parentID);
-updateWorkDesc(pWorkDesc, capId);
