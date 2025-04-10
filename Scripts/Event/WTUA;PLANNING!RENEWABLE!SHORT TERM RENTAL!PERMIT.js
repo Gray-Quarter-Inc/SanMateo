bb6936d32b2ee65777d863cc8a152e1b2251629e
Deanna Hoops <deanna@grayquarter.com>
2025-04-10 08:31:50 -0400

    Update WTUA;PLANNING!RENEWABLE!SHORT TERM RENTAL!PERMIT.js

--- Scripts/Event/WTUA;PLANNING!RENEWABLE!SHORT TERM RENTAL!PERMIT.js
+++ Scripts/Event/WTUA;PLANNING!RENEWABLE!SHORT TERM RENTAL!PERMIT.js
@@ -1,5 +1,5 @@
 if (wfTask == "Planning Review" && wfStatus == "Approved") {
     lic = new licenseObject(null, capId);
     lic.setStatus("Active");
-    lic.setExpiration(dateAdd(wfDateMMDDYYYY, 36));
+    lic.setExpiration(dateAddMonths(wfDateMMDDYYYY, 36));
 }
