008ea64a35390ddfc0a062005a95f9df67ebfdc7
Ray Schug <10674678+RSchug@users.noreply.github.com>
2025-04-11 11:29:00 -0700

    PageFlow Cleanup & Fix BLD_RES_NEW_OnLoad_LocationInfo - serialize

--- Scripts/Pageflow/BLD_RES_NEW_OnLoad_LocationInfo.js
+++ Scripts/Pageflow/BLD_RES_NEW_OnLoad_LocationInfo.js
@@ -77,23 +77,23 @@ if (true) {
 }
 
 var currentUserID = aa.env.getValue("CurrentUserID");
-Avo_LogDebug("CurrentUserID(" + currentUserID + ")", 2);    //debug
+Avo_LogDebug("CurrentUserID: " + currentUserID, 2);    //debug
 var parentCapIdString = String(cap.getParentCapID());
 var parentCapId;
 if (parentCapIdString) {
     var parentCapIdArr = parentCapIdString.split("-");
     parentCapId = aa.cap.getCapID(parentCapIdArr[0], parentCapIdArr[1], parentCapIdArr[2]).getOutput();
 }
-Avo_LogDebug("parentCapIdString(" + parentCapIdString + ")", 2);    //debug
+Avo_LogDebug("parentCapIdString: " + parentCapIdString, 2);    //debug
 
 var publicUserID = null;
 var publicUser = false;
 var publicUserEmail = "";
 var publicUser = false;
 if (currentUserID.indexOf("PUBLICUSER") == 0) { // ignore public users
-	publicUserID = currentUserID;
-	currentUserID = "ADMIN";
-	publicUser = true;
+    publicUserID = currentUserID;
+    currentUserID = "ADMIN";
+    publicUser = true;
 }
 logDebug("currentUserID: " + currentUserID + ", publicUserID: " + publicUserID);
 if (publicUserID) {
@@ -107,11 +107,11 @@ if (publicUserID) {
     }
 }
 var debugEmailTo = "rschug@truepointsolutions.com";
-if (publicUserEmail.toLowerCase().indexOf("@_") >= 0) publicUserEmail = publicUserEmail.replace("@_","@");
-if (publicUserEmail.toLowerCase().indexOf("turned_off") >= 0) publicUserEmail = publicUserEmail.replace("turned_off","");
-if (publicUserEmail.toLowerCase().indexOf("turnedoff") >= 0) publicUserEmail = publicUserEmail.replace("turnedoff","");
+if (publicUserEmail.toLowerCase().indexOf("@_") >= 0) publicUserEmail = publicUserEmail.replace("@_", "@");
+if (publicUserEmail.toLowerCase().indexOf("turned_off") >= 0) publicUserEmail = publicUserEmail.replace("turned_off", "");
+if (publicUserEmail.toLowerCase().indexOf("turnedoff") >= 0) publicUserEmail = publicUserEmail.replace("turnedoff", "");
 if (publicUserEmail.toLowerCase().indexOf("@truepointsolutions.com") >= 0) debugEmailTo = publicUserEmail;
-if (publicUserEmail.toLowerCase().indexOf("lindacharron13@gmail.com") >= 0) debugEmailTo = "lcharron@truepointsolutions.com;rschug@truepointsolutons.com";
+if (publicUserEmail.toLowerCase().indexOf("lindacharron13@gmail.com") >= 0) debugEmailTo = "lcharron@truepointsolutions.com";
 
 var AInfo = new Array();						// Create array for tokenized variables
 loadAppSpecific4ACA(AInfo); 					// Add AppSpecific Info
@@ -120,38 +120,40 @@ loadAppSpecific4ACA(AInfo); 					// Add AppSpecific Info
 | Main Loop
 /-----------------------------------------------------------------------------------------------------*/
 try {
+    Avo_LogDebug("capID:" + cap.capID, 2);  //debug
     //Script 355 Residential Revision Copy APO
     // include("BLD_355_OnLoad_ResRevCopyApo");
     var parcelCap = null, parentAltId = "";
     if (parentCapId) {
         var parentCap = aa.cap.getCapViewBySingle4ACA(parentCapId);
         var parentAltId = aa.cap.getCap(parentCapId).getOutput().capModel.altID;
-        Avo_LogDebug("Parent(" + parentAltId + ")", 2);  //debug
+        Avo_LogDebug("parentAltId: " + parentAltId + ", parcentCap: " + parentCap, 2);  //debug
     } else {
-        Avo_LogDebug("parentCapId(" + parentCapId + ")", 2);  //debug
+        Avo_LogDebug("parentCapId: " + parentCapId, 2);  //debug
     }
 
-    if (parcelCap) {
+    if (parentCap) {
+        Avo_LogDebug("Processing parent: " + parentAltId + ", parentCapId: " + parentCapId + ", parcentCap: " + parentCap, 2);  //debug
         // Copy Addresses
         var addrModel = parentCap.addressModel;
         if (addrModel) {
             addrModel.capID = cap.capID;
-
             cap.addressModel = addrModel;
             Avo_LogDebug("Copied address " + addrModel.displayAddress + " from parent " + parentAltId, 1);  //debug
         }
 
         var allAddrs = parentCap.getAddressModels();
         if (allAddrs && allAddrs.size() > 0) {
-            Avo_LogDebug("Total Addresses(" + allAddrs.size() + ")", 2);    //debug
+            Avo_LogDebug("Total Addresses: " + allAddrs.size(), 2);    //debug
 
             for (var i = 0; i < allAddrs.size(); i++) {
                 var addrModel = allAddrs.get(i);
-                Avo_LogDebug(serialize(addrModel), 2);    //debug
+                // Avo_LogDebug(serialize(addrModel), 2);    //debug
+                Avo_LogDebug("addressModel: " + describe(addrModel), 2);    //debug
 
                 addrModel.capID = cap.capID;
 
-                Avo_LogDebug("Address(" + addrModel.displayAddress + ")", 2);    //debug
+                Avo_LogDebug("Address: " + addrModel.displayAddress, 2);    //debug
             }
 
             cap.setAddressModels(allAddrs);
@@ -169,14 +171,14 @@ try {
 
         var allParcels = parentCap.getParcelList();
         if (allParcels && allParcels.size() > 0) {
-            Avo_LogDebug("Total Parcels(" + allParcels.size() + ")", 2);    //debug
+            Avo_LogDebug("Total Parcels: " + allParcels.size(), 2);    //debug
 
             for (var i = 0; i < allParcels.size(); i++) {
                 var parcelModel = allParcels.get(i);
 
                 parcelModel.capIDModel = cap.capID;
 
-                Avo_LogDebug("Parcel(" + parcelModel.parcelNumber + ")", 2);    //debug
+                Avo_LogDebug("Parcel: " + parcelModel.parcelNumber, 2);    //debug
             }
 
             cap.setParcelList(allParcels);
@@ -200,14 +202,14 @@ try {
 
         var allOwners = parentCap.capOwnerList;
         if (allOwners && allOwners.size() > 0) {
-            Avo_LogDebug("Total Owners(" + allOwners.size() + ")", 2);    //debug
+            Avo_LogDebug("Total Owners: " + allOwners.size(), 2);    //debug
 
             for (var i = 0; i < allOwners.size(); i++) {
                 var refOwnerModel = allOwners.get(i);
 
                 refOwnerModel.capID = cap.capID;
 
-                Avo_LogDebug("Owner(" + refOwnerModel.ownerFullName + ")", 2);    //debug
+                Avo_LogDebug("Owner: " + refOwnerModel.ownerFullName, 2);    //debug
             }
 
             cap.capOwnerList = allOwners;
