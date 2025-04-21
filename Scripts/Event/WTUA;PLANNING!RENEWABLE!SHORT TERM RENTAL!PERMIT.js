5b278bdbc3458614888d682be02c86bc9dc56b9c
LindaCharron <linda@grayquarter.com>
2025-04-21 16:25:34 -0700

    Update WF Task Name for STR Workflow

--- Scripts/Event/WTUA;PLANNING!RENEWABLE!SHORT TERM RENTAL!PERMIT.js
+++ Scripts/Event/WTUA;PLANNING!RENEWABLE!SHORT TERM RENTAL!PERMIT.js
@@ -1,4 +1,4 @@
-if (wfTask == "Planning Review" && wfStatus == "Approved") {
+if (wfTask == "Project Analysis" && wfStatus == "Approved") {
     lic = new licenseObject(null, capId);
     lic.setStatus("Active");
     lic.setExpiration(dateAddMonths(wfDateMMDDYYYY, 36));
